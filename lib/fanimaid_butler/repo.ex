e9724388fae9c78cf257f570c569a122ce0309e6
Sidney Keese <sidke.z@gmail.com>
2019-06-06 09:37:12 -0500

    mix deps.update and required code changes

--- lib/fanimaid_butler/repo.ex
+++ lib/fanimaid_butler/repo.ex
@@ -1,4 +1,6 @@
 defmodule FanimaidButler.Repo do
-  use Ecto.Repo, otp_app: :fanimaid_butler
+  use Ecto.Repo,
+    otp_app: :fanimaid_butler,
+    adapter: Ecto.Adapters.Postgres
   use Scrivener, page_size: 10
 end
