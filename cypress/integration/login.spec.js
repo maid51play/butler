493cb0dde56d25a69d2bdd779a0712c32251200d
MeganeMidori <MeganeMidori@@users.noreply.github.com>
2019-08-15 12:57:53 -0700

    wip login spec

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,41 @@
+/// <reference types="Cypress" />
+
+context('Login', () => {
+  beforeEach(() => {
+    cy.visit('localhost:4000/')
+  })
+
+  it('displays an error when username and password are incorrect', () => {
+    cy.get('#user_username')
+      .type('fake@email.com').should('have.value', 'fake@email.com')
+
+    cy.get('#user_password')
+      .type('password')
+
+    cy.contains('Submit')
+      .click()
+      
+    cy.contains('Incorrect username or password').should('be.visible')
+
+    cy.contains('Cafe Data').click()
+
+    cy.contains('unauthenticated').should('be.visible')
+  })
+
+  it('authenticates when username and password are correct', () => {
+    cy.get('#user_username')
+      .type('admin').should('have.value', 'admin')
+
+    cy.get('#user_password')
+      .type('password')
+
+    cy.contains('Submit')
+      .click()
+      
+    cy.contains('Logout').should('be.visible')
+
+    cy.contains('Cafe Data').click()
+
+    cy.contains('Total Reservations').should('be.visible')
+  })
+})
