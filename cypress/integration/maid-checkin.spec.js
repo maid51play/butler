28fc355b438bf201fd58a6e2c086ac5f565cfa4f
Midori <51420500+MeganeMidori@users.noreply.github.com>
2019-11-12 12:23:33 -0800

    maid checkin cypress test (#57)
    * maid checkin happy path test

---
+++ cypress/integration/maid-checkin.spec.js
@@ -0,0 +1,50 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+// <reference types="Cypress" />
+
+context('Maid Checkin', () => {
+  beforeEach(() => {
+    cy.factorydb('party', {});
+
+    cy.login();
+    cy.visit('localhost:4001/');
+  });
+
+  it('maid check in happy path', () => {
+    cy.contains('Maid Check In').click();
+
+    cy.contains('Maid Check-In').should('be.visible');
+
+    cy.contains('New maid').click();
+
+    cy.contains('New maid').should('be.visible');
+
+    cy.get('#maid_name')
+      .type('Mikuru').should('have.value', 'Mikuru');
+
+    cy.contains('Submit').click();
+
+    cy.contains('Maid Mikuru created successfully.').should('be.visible');
+
+    cy.contains('td', 'Mikuru').parent().contains('Check In').click();
+
+    cy.contains('td', 'Mikuru').parent().contains('0:00').should('be.visible');
+
+    cy.clock(new Date().getTime() + 606000);
+
+    cy.contains('Maid Check In').click();
+
+    cy.contains('td', 'Mikuru').parent().contains('0:10').should('be.visible');
+
+    cy.contains('Tables').click();
+
+    cy.contains('Book').click();
+
+    cy.contains('Mikuru').should('be.visible');
+
+    cy.contains('Maid Check In').click();
+
+    cy.contains('td', 'Mikuru').parent().contains('Check Out').click();
+
+    cy.contains('Mikuru checked out successfully.').should('be.visible');
+  });
+});
