9a2d03f2f4c60bb1467a435b7048751444431f32
MeganeMidori <MeganeMidori@users.noreply.github.com>
2021-05-07 23:06:31 -0700

    add ical link

--- web/models/reservation.ex
+++ web/models/reservation.ex
@@ -14,11 +14,11 @@ defmodule Butler.Reservation do
 
   @derive {
     Jason.Encoder,
-    only: [:id, :name, :size, :time_waitlisted, :time_in, :seat_alone, :notes, :seat_alone, :maid]
+    only: [:id, :name, :size, :time_waitlisted, :time_in, :seat_alone, :notes, :maid]
   }
   @derive {
     Poison.Encoder,
-    only: [:id, :name, :size, :time_waitlisted, :time_in, :seat_alone, :notes, :seat_alone, :maid]
+    only: [:id, :name, :size, :time_waitlisted, :time_in, :seat_alone, :notes, :maid]
   }
   schema "reservations" do
     field :name, :string
