0e2ae92584466f3bf6d531d96123f3121f5c3bd1
MeganeMidori <MeganeMidori@users.noreply.github.com>
2021-09-20 22:37:47 -0700

    versioning?

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Butler.Mixfile do
     [
       app: :butler,
       version: "0.0.1",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
