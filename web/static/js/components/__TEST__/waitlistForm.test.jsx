bf8faa225d9ce59c5a9d9c910b53730dc0fa089d
MeganeMidori <MeganeMidori@users.noreply.github.com>
2021-11-15 21:12:39 -0800

    ci

--- web/static/js/components/__TEST__/waitlistForm.test.jsx
+++ web/static/js/components/__TEST__/waitlistForm.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { render, screen, hasInputValue } from '@testing-library/react';
-import { within } from '@testing-library/dom';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import WaitlistFormComponent from '../waitlistForm';
 
